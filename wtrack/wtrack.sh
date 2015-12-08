88ebe5f4d79758f36eeecd8571b085fa0e74ba50
wangxb <wxb0328@gmail.com>
2015-12-08 17:49:15 +0800

    usage

--- wtrack/wtrack.sh
+++ wtrack/wtrack.sh
@@ -3,7 +3,7 @@
 if [ $# -lt 1 ];then
   printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n"
   printf "NAME \n\twtrack -- Recursive search characters in files in the specified directory\n"
-  printf "SYNOPSIS \n\t bash wtrack.sh search [file-suffix] [dir]\n"
+  printf "SYNOPSIS \n\t bash wtrack.sh [OPTIONS] search [file-suffix] [dir]\n"
   printf "OPTIONS \n\t -git check 'echo','print_r','var_dump','die','exit'\n"
   printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n"
 fi
@@ -11,5 +11,4 @@ fi
 
 
 
-case $1 in
 
