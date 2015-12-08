8e344f51f20d51fc63e6d59d1a493e7992b6ed9c
wangxb <wxb0328@gmail.com>
2015-12-08 17:36:57 +0800

    stage

---
+++ wtrack/wtrack.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -lt 1 ];then
+  printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n"
+  printf "NAME \n\twtrack -- Recursive search characters in files in the specified directory\n"
+  printf "SYNOPSIS \n\t bash wtrack.sh search [file-suffix] [dir]\n"
+  printf "OPTIONS \n\t -git check 'echo','print_r','var_dump','die','exit'\n"
+  printf "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n"
+fi
+
+
+
+
+case $1 in
+
